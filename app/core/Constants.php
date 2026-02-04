ff7f2a09109a3477cb656e7ec49763ccb7a9db5d
allweDepth <dpupr.pasangkayu@gmail.com>
2026-02-04 17:55:17 +0800

    ubah base url

--- app/core/Constants.php
+++ app/core/Constants.php
@@ -3,8 +3,8 @@
 //define('BASEURL', 'http://localhost/sesendokneo/public/');
 //define('pathURL', 'http://localhost/sesendokneo/public/');
 define('LOCALURL', 'http://192.168.1.6:8085/');
-define('BASEURL', 'http://192.168.1.6:8085/');
-define('pathURL', 'http://192.168.1.6:8085/');
+define('BASEURL', 'http://192.168.1.6:8085/public/');
+define('pathURL', 'http://192.168.1.6:8085/public/');
 $msx = rand(32, 64);
 $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ()<>?{}#$&=-*^@';
 $keyEnc = substr(str_shuffle($permitted_chars), 0, $msx);
