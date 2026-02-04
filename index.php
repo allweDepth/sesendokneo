c31949aaa3375855f2fa1d263e3b5e70df67d000
allweDepth <dpupr.pasangkayu@gmail.com>
2026-02-04 14:40:34 +0800

    hufff sialan AI tidak menyelesaikan masalah

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
 //header("Location: public");
-header("Location: /");
-exit;
+//header("Location: /");
+//exit;
+session_start();
+require_once '../app/core/Constants.php';
+require_once '../app/core/App.php';
+
+$app = new App();
