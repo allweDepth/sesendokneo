34a5e60fce98d75318301064bf060a5ebb0fe1e7
allweDepth <dpupr.pasangkayu@gmail.com>
2026-02-04 14:46:03 +0800

    huft

--- index.php
+++ index.php
@@ -1,6 +1,2 @@
 <?php
-//header("Location: public");
-echo "<h1>INDEX.PHP MASUK</h1>";
-require_once '../app/core/Constants.php';
-require_once '../app/core/App.php';
-$app = new App();
+header("Location: public");
