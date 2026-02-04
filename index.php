eaee0932842d11408c9196ac780ca83cc6ba0dac
allweDepth <dpupr.pasangkayu@gmail.com>
2026-02-04 14:43:19 +0800

    balik lagi

--- index.php
+++ index.php
@@ -1,9 +1,2 @@
 <?php
-//header("Location: public");
-//header("Location: /");
-//exit;
-session_start();
-require_once '../app/core/Constants.php';
-require_once '../app/core/App.php';
-
-$app = new App();
+header("Location: public");
