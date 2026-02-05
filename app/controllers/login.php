417eb8c0b798cba1e606fd107e317bda1faff605
allweDepth <dpupr.pasangkayu@gmail.com>
2026-02-05 15:21:38 +0800

    indek ubah

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -2,28 +2,37 @@
 class Login extends Controller
 {
     public function index()
-    {
-        
-        if (isset($_SESSION["user"])) {
-            unset($_SESSION["user"]);
-        }
+{
+    if (session_status() === PHP_SESSION_NONE) {
         session_start();
-        $key_encrypt = $this->scriptConstruct("query",['jns'=>'key_encrypt','tbl'=>'key_encrypt'])->key_encrypt();
-        $_SESSION["key_encrypt"] = $key_encrypt;
-        $_SESSION["tesbede"] = $key_encrypt;
-        $dataHeader['awalHeader'] = '';
-        $dataHeader['title'] = '| Login';
-        $dataHeader['css'] = 'css/login.css';
-        $dataHeader['tambahan_css'] = '';
-        $dataFooter['js'] = 'js/login.js';
-        $dataFooter['tambahan_js'] = '';
-        $dataFooter['dok'] = 'Login';
-        $dataFooter['key_encrypt'] = $key_encrypt;
-        $this->view('templates/header_login', $dataHeader);
-        $this->view('login/index');
-        $this->view('templates/footer_modal');
-        $this->view('templates/footer', $dataFooter);
     }
+
+    if (isset($_SESSION["user"])) {
+        unset($_SESSION["user"]);
+    }
+
+    $key_encrypt = $this->scriptConstruct("query",[
+        'jns'=>'key_encrypt',
+        'tbl'=>'key_encrypt'
+    ])->key_encrypt();
+
+    $_SESSION["key_encrypt"] = $key_encrypt;
+
+    $dataHeader['awalHeader'] = '';
+    $dataHeader['title'] = '| Login';
+    $dataHeader['css'] = 'css/login.css';
+    $dataHeader['tambahan_css'] = '';
+
+    $dataFooter['js'] = 'js/login.js';
+    $dataFooter['tambahan_js'] = '';
+    $dataFooter['dok'] = 'Login';
+    $dataFooter['key_encrypt'] = $key_encrypt;
+
+    $this->view('templates/header_login', $dataHeader);
+    $this->view('login/index');
+    $this->view('templates/footer_modal');
+    $this->view('templates/footer', $dataFooter);
+}
     public function masuk()
     { 
         $data = $this->script("masuk")->masuk();
