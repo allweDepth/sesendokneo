0f8f6146bbe3371e0babba3e428752c275a4d4ef
allweDepth <dpupr.pasangkayu@gmail.com>
2026-02-08 13:45:30 +0800

    Constant synology

--- app/core/Constants.php
+++ app/core/Constants.php
@@ -1,7 +1,10 @@
 <?php
-define('LOCALURL', 'http://localhost/sesendokneo/');
-define('BASEURL', 'http://localhost/sesendokneo/public/');
-define('pathURL', 'http://localhost/sesendokneo/public/');
+// define('LOCALURL', 'http://localhost/sesendokneo/');
+// define('BASEURL', 'http://localhost/sesendokneo/public/');
+// define('pathURL', 'http://localhost/sesendokneo/public/');
+define('LOCALURL', 'http://192.168.1.6:8085/');
+define('BASEURL', 'http://192.168.1.6:8085/public/');
+define('pathURL', 'http://192.168.1.6:8085/public/');
 $msx = rand(32, 64);
 $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ()<>?{}#$&=-*^@';
 $keyEnc = substr(str_shuffle($permitted_chars), 0, $msx);
