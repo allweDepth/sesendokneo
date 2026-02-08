ae026362b1f5035dfa0c1ddcb7b5bdceadc966f3
allweDepth <dpupr.pasangkayu@gmail.com>
2026-02-08 17:13:46 +0800

    session yang diubah

--- app/core/Constants.php
+++ app/core/Constants.php
@@ -2,6 +2,15 @@
 // define('LOCALURL', 'http://localhost/sesendokneo/');
 // define('BASEURL', 'http://localhost/sesendokneo/public/');
 // define('pathURL', 'http://localhost/sesendokneo/public/');
+// define('LOCALURL', 'http://192.168.1.6:8085/');
+// define('BASEURL', 'http://192.168.1.6:8085/public/');
+// define('pathURL', 'http://192.168.1.6:8085/public/');
+
+ini_set('session.save_path', '/volume1/web/sesendokneo/sessions');
+ini_set('session.cookie_secure', '0');      // wajib karena masih HTTP
+ini_set('session.cookie_httponly', '1');
+
+
 define('LOCALURL', 'http://192.168.1.6:8085/');
 define('BASEURL', 'http://192.168.1.6:8085/public/');
 define('pathURL', 'http://192.168.1.6:8085/public/');
