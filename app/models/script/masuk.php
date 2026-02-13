b34745719a707de67b75f68eb9fb615a4b77e773
allweDepth <dpupr.pasangkayu@gmail.com>
2026-02-13 12:38:55 +0800

    masuk

--- app/models/script/masuk.php
+++ app/models/script/masuk.php
@@ -3,103 +3,75 @@ class Masuk
 {
     public function masuk()
     {
+        // Mulai session jika belum
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
-        // var_dump($_POST);
+
+        // Include init
         require 'init_no_session.php';
+
+        // Hapus user lama jika ada
         if (isset($_SESSION["user"])) {
             unset($_SESSION["user"]);
         }
 
         $DB = DB::getInstance();
-        $keyEncrypt = $_SESSION['key_encrypt'];
+        $keyEncrypt = isset($_SESSION['key_encrypt']) ? $_SESSION['key_encrypt'] : null;
         $user = new User();
         $validate = new Validate($_POST);
-        //$crypto = new CryptoUtils();
-        // var_dump($_POST);
-        // var_dump($validate);
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // var_dump($_POST);
+
+            // Validasi input
             $username = $validate->setRules('username', 'Username', [
                 'sanitize' => 'string',
                 'required' => true,
                 'min_char' => 6
             ]);
-            // var_dump($username);
-            //$username = $crypto->decrypt($username, $keyEncrypt);
-            $password = $validate->setRules('password', 'Pasword', [
+
+            $password = $validate->setRules('password', 'Password', [
                 'sanitize' => 'string',
                 'required' => true,
                 'min_char' => 4
             ]);
 
-            // var_dump($password);
-            // $password = password_hash($password, PASSWORD_DEFAULT);
-            // var_dump(password_hash($password, PASSWORD_DEFAULT));
-            //$password = $crypto->decrypt($password, $keyEncrypt);
             if ($validate->passed()) {
+                // Ambil data user
+                $dataArr = $DB->getQuery(
+                    'SELECT * FROM user_sesendok_biila WHERE (username = ? OR email = ?)',
+                    [$username, $username]
+                );
 
-                $data = $DB->getQuery('SELECT * FROM user_sesendok_biila WHERE (username = ? OR email = ?)', [$username, $username])[0];
-                // var_dump($data);
-                $passIsValid = password_verify($password, $data->password);
-                // var_dump("passIsValid = $passIsValid");
-                //var_dump(sizeof((array)$data));
-                if (sizeof((array)$data) > 0 && $data->disable_login <= 0 && $passIsValid) { // ubah ke array sebelum menggunakan sizeof
-                    $pesanError = '';
-                    //var_dump($data->disable_login);
-                    //$pesanError = $user->validasiLogin($_POST);
-                    if (empty($pesanError)) {
-                        $status = session_status();
-                        if ($status == PHP_SESSION_NONE) {
-                            //There is no active session
-                            session_destroy();
-                        } else
-                        if ($status == PHP_SESSION_DISABLED) {
-                            //Sessions are not available
-                            session_destroy();
-                        } else
-                        if ($status == PHP_SESSION_ACTIVE) {
-                            //Destroy current and start new one
-                            session_destroy();
-                            session_start();
-                        }
-                        $_SESSION["user"] = (array)$data;
-                        file_put_contents(
-                            '/volume1/web/sesendokneo/debug_session.txt',
-                            print_r($_SESSION, true)
-                        );
-                        $_SESSION["user"]["key_encrypt"] = KEY_ENCRYPT;
-                        //var_dump($_SESSION);
-                        $id = $_SESSION["user"]["id"];
-                        //var_dump($id);
-                        $type_user = $_SESSION["user"]["type_user"];
-                        //$DB->update('user_sesendok_biila', ['tgl_login'=>NOW()], ['id','=',$id]);
-                        $DB->runQuery('UPDATE `user_sesendok_biila` SET tgl_login = NOW() WHERE id = ?', [$id]);
-                        if ($type_user == 'admin') {
-                            $session = 1;
-                            // header("Location: home");
-                            // exit;
-                        } else {
-                            $session = 1; //$session = 2;
-                            // header("Location: home");
-                            // exit();
-                        }
-                    } else {
-                        $session = 4;
-                    }
-                } elseif ($data->disable_login > 0) {
-                    $session = 6;
-                } else {
-                    $session = 5;
+                if (count($dataArr) === 0) {
+                    return 7; // login gagal
                 }
+
+                $data = $dataArr[0];
+                $passIsValid = password_verify($password, $data->password);
+
+                if ($passIsValid && $data->disable_login <= 0) {
+                    // Set session user
+                    $_SESSION["user"] = (array)$data;
+                    $_SESSION["user"]["key_encrypt"] = KEY_ENCRYPT;
+
+                    // Update tgl_login
+                    $DB->runQuery('UPDATE `user_sesendok_biila` SET tgl_login = NOW() WHERE id = ?', [$data->id]);
+
+                    // Return kode sukses
+                    return 1; // login sukses (admin/umum)
+                } elseif ($data->disable_login > 0) {
+                    return 6; // akun nonaktif
+                } else {
+                    return 7; // login gagal
+                }
+
             } else {
-                $session = $validate->getError();
-                // var_dump($validate->getError());
+                return 4; // validasi error
             }
+
         } else {
-            $session = 7;
+            return 7; // request bukan POST
         }
-        return $session;
     }
 }
